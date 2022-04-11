08dd7a9355be9be2d8dbac35f727c4b559beb276
binrohan <binrohan.cs@gmail.com>
2022-04-11 10:45:42 +0600

    unused filed remove

--- IqraCommerce/IqraCommerce/Models/CustomerArea/CustomerModel.cs
+++ IqraCommerce/IqraCommerce/Models/CustomerArea/CustomerModel.cs
@@ -1,63 +1,24 @@
-﻿using IqraBase.Data;
+﻿
+using IqraBase.Data.Entities;
+using IqraBase.Data;
 using IqraBase.Data.Models;
 using System;
-using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IqraCommerce.Data;
 
-namespace EBonik.Data.Models.CustomerArea
+namespace EBonik.Data.Models.UserArea
 {
-    [Table("Customer")]
-    [Alias("cstmr")]
     public class CustomerModel : DropDownBaseModel
     {
-        [Required]
+        public string ImageURL { get; set; }
         public string Phone { get; set; }
-        /// <summary>
-        /// Optional
-        /// </summary>
         public string Email { get; set; }
-        [Required]
-        [StringLength(20, MinimumLength = 4)]
         public string Password { get; set; }
-        /// <summary>
-        /// Image Path
-        /// </summary>
-        public string Image { get; set; }
-        /// <summary>
-        /// OtpMessage.Id
-        /// </summary>
-        public Guid OTPId { get; set; }
-        /// <summary>
-        /// ActivityId
-        /// </summary>
-    }
-
-    public class UpdateCustomerInfoModel
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        /// <summary>
-        /// ActivityId
-        /// </summary>
-    }
-    public class UpdateCustomerPasswordModel
-    {
-        public Guid Id { get; set; }
-        [Required]
-        [StringLength(20, MinimumLength = 4)]
-        public string Password { get; set; }
-    }
-    public class CustomerCreateModel : DropDownBaseModel
-    {
-        [Required]
-        public string Phone { get; set; }
-        /// <summary>
-        /// Optional
-        /// </summary>
-        public string Email { get; set; }
-        [Required]
-        [StringLength(20, MinimumLength = 3)]
-        public string Password { get; set; }
+        public string DueAmount { get; set; }
+        public string Cashback { get; set; }
     }
 }
