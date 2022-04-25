3f5811137a72ac83b39d6127202878e3bb69643b
binrohan <binrohan.cs@gmail.com>
2022-04-25 10:06:23 +0600

    Address Name field added in CreateDto

--- Client/IqraCommerce.API/DTOs/Address/AddressUpdateDto.cs
+++ Client/IqraCommerce.API/DTOs/Address/AddressUpdateDto.cs
@@ -8,10 +8,12 @@ namespace IqraCommerce.API.DTOs
         public Guid Id { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
+        public string Name { get; set; }
         public Guid ProvinceId { get; set; }
         public Guid DistrictId { get; set; }
         public Guid UpazilaId { get; set; }
         public bool IsPrimary { get; set; }
         public string Remarks { get; set; }
+        public Guid ActivityId { get; set; }
     }
 }
